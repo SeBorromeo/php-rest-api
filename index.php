453f98748b39452ed074ed3b8eca81b965ba7165
Sebastian Borromeo <uwg3xs@virginia.edu>
2026-01-19 20:43:10 -0500

    More informative documentation given w base route response (/api)

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php require __DIR__ . '/vendor/autoload.php';
 
 header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Authorization, Content-Type");
 header('Content-Type: application/json');
 
@@ -17,10 +18,54 @@ App::useMiddleware([BodyParser::class, 'urlencoded']);
 
 // API Documentation at base route /api/
 App::get('/', function (Request $req, Response $res) { 
-    $res->toJSON(['message' => "API by Sebastian Borromeo",
+    $res->toJSON(['name' => "API by Sebastian Borromeo",
+        "version" => "1.0.0",
         'endpoints' => [
-            ['path' => '/api/auth/validateToken', 'description' => 'Validate Bearer Token'],
-            ['path' => '/api/auth/login', 'description' => 'POST with credentials to get token'],
+            'merch' => [
+                'description' => 'Merchandise resources',
+                'routes' => [
+                    [
+                        'method' => 'GET',
+                        'path' => '/merch',
+                        'description' => 'Get full list of merch'
+                    ],
+                    [
+                        'method' => 'GET',
+                        'path' => '/merch/{id}',
+                        'description' => 'Get merch by ID'
+                    ],
+                    [
+                        'method' => 'POST',
+                        'path' => '/merch',
+                        'description' => 'Create new merch item'
+                    ]
+                ]
+                ],
+                'auth' => [
+                'description' => 'Authentication and authorization',
+                'routes' => [
+                    [
+                        'method' => 'GET',
+                        'path' => '/auth/validateToken',
+                        'description' => 'Validate authorization token'
+                    ],
+                    [
+                        'method' => 'GET',
+                        'path' => '/auth/extendToken',
+                        'description' => 'Issue new authorization token given a valid token'
+                    ],
+                    [
+                        'method' => 'POST',
+                        'path' => '/auth/login',
+                        'description' => 'Get authorization token from credentials'
+                    ],
+                    [
+                        'method' => 'POST',
+                        'path' => '/auth/logout',
+                        'description' => 'Invalidate authorization token'
+                    ]
+                ]
+                ]
         ]
     ]);
     exit;
