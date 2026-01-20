0f9dff70092948a3041c9227889ac056032270c9
Sebastian Borromeo <uwg3xs@virginia.edu>
2026-01-20 02:56:36 -0500

    createInsertStmt for MerchController

--- src/Lib/DBConnect.php
+++ src/Lib/DBConnect.php
@@ -11,8 +11,19 @@ class DBConnect {
 	}
 
 	public static function createDB() {
-		$conn = new \PDO('mysql:host=' . $_ENV['DB_HOST'] .';dbname=' . $_ENV['DB_NAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+		$conn = new \PDO('mysql:host=' . $_ENV['DB_HOST'] . ';port=' . $_ENV['DB_PORT'] . ';dbname=' . $_ENV['DB_NAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
 		$conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 		return $conn;
+	}
+
+	public static function createInsertStmt(string $tableName, array $columns) {
+		$placeholders = array_map(function($col) { return ':' . $col; }, $columns);
+
+		return sprintf(
+            'INSERT INTO %s (%s) VALUES (%s)',
+            $tableName,
+            implode(', ', $columns),
+            implode(', ', $placeholders)
+        );
 	}
 }
